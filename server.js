38bcc0f5dbc9d4e8ce27a98ed3d0a9950989fe03
mattspoche <matthewspoche@comcast.net>
2018-12-22 18:45:15 -0600

    added scrape route

--- server.js
+++ server.js
@@ -21,7 +21,25 @@ var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines
 mongoose.connect(MONGODB_URI, { useNewUrlParser: true });
 
 //Routes
+app.get("/scrape", (req, res) => {
+    let result = {};
+    axios.get("https://www.cnn.com/").then(response => {
+        let $ = cheerio.load(response.data);
 
+        $("article").each(function(i, element) {
+            //empty result object
+
+            result.title = $(this)
+                .children("cd__headline-text")
+                .text();
+            result.link = $(this)
+                .children("a")
+                .attr("href");
+                
+            });
+        });
+        res.json(result);
+    });
 
 //Start the server
 app.listen(PORT, function() {
